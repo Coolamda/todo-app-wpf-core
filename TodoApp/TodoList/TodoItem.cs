5cf956241bac8d7aafbebe2044a2370c0b5ae25e
Liam Wohlfart <liam.wohlfart0@gmail.com>
2020-09-25 08:50:43 +0200

    Add comments to TodoItem and TodoList

--- TodoApp/TodoList/TodoItem.cs
+++ TodoApp/TodoList/TodoItem.cs
@@ -1,15 +1,40 @@
 ﻿namespace WpfCore.TodoList
 {
+    /// <summary>
+    /// A simple TodoItem class.
+    /// </summary>
     public class TodoItem
     {
+        #region Properties
+
+        /// <summary>
+        /// Keeps track for the amount of todos created (simulating increment id).
+        /// </summary>
         private static int _todoItemCount = 0;
 
+        /// <summary>
+        /// Id of the todo.
+        /// </summary>
         public int Id;
 
+        /// <summary>
+        /// Description of the todo.
+        /// </summary>
         public string Description { get; set; } = null;
 
+        /// <summary>
+        /// Completion status of the todo.
+        /// </summary>
         public bool IsCompleted { get; set; } = false;
 
+        #endregion Properties
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a TodoItem with a description. Use total amount of created todos as id. Increment todoItemCount.
+        /// </summary>
+        /// <param name="description">Description of the new todo</param>
         public TodoItem(string description)
         {
             Description = description;
@@ -17,15 +42,18 @@
             _todoItemCount++;
         }
 
-        public TodoItem(string description, bool isCompleted)
-        {
-            Description = description;
-            IsCompleted = isCompleted;
-        }
+        #endregion Constructor
 
+        #region Methods
+
+        /// <summary>
+        /// Toggle the completion status.
+        /// </summary>
         public void ToggleTodo()
         {
             IsCompleted = !IsCompleted;
         }
+
+        #endregion Methods
     }
 }
--- TodoApp/TodoList/TodoList.cs
+++ TodoApp/TodoList/TodoList.cs
@@ -2,10 +2,27 @@
 
 namespace WpfCore.TodoList
 {
+    /// <summary>
+    /// A simple TodoList. User can add and remove TodoItems to the list.
+    /// </summary>
     public class TodoList
     {
+        #region Properties
+
+        /// <summary>
+        /// A list of TodoItems.
+        /// </summary>
         public List<TodoItem> TodoItems = new List<TodoItem>();
 
+        #endregion Properties
+
+        #region Methods
+
+        /// <summary>
+        /// Adds an TodoItem to Todoitems.
+        /// </summary>
+        /// <param name="description">Description of the new todo.</param>
+        /// <returns></returns>
         public TodoItem AddTodo(string description)
         {
             var todoItem = new TodoItem(description);
@@ -13,14 +30,24 @@ namespace WpfCore.TodoList
             return todoItem;
         }
 
+        /// <summary>
+        /// Removes a TodoItem in TodoItems by using an id.
+        /// </summary>
+        /// <param name="id">Id of todo which should be deleted.</param>
         public void RemoveTodoAtId(int id)
         {
             TodoItems.RemoveAt(id);
         }
 
+        /// <summary>
+        /// Removes a TodoItem by using a reference.
+        /// </summary>
+        /// <param name="todoItem">Reference of TodoItem which should be deleted.</param>
         public void RemoveTodo(TodoItem todoItem)
         {
             TodoItems.Remove(todoItem);
         }
+
+        #endregion Methods
     }
 }
